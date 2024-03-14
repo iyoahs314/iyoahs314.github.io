5ab9833b88cdf96fd0dfa66bf372b9e77efd397d
iyoahs314 <161724594+iyoahs314@users.noreply.github.com>
2024-03-14 21:25:50 +0800

    1.1.0

--- script/index-left-slider.js
+++ script/index-left-slider.js
@@ -9,9 +9,9 @@ let prev = 3;
 let next = 1; 
 
 body.style.backgroundImage = 'url("pic/background'+(current+1)+'.gif")';
-change_slider(3, "rgb(152, 209, 188)", "rgb(0, 83, 3)");
+change_slider(3, "rgb(244, 230, 154)", "rgb(123, 104, 0)");
 change_slider(2, "rgb(208, 175, 225)", "rgb(105, 3, 160)");
-change_slider(1, "rgb(244, 248, 171)", "rgb(92, 97, 0)");
+change_slider(1, "rgb(152, 209, 188)", "rgb(0, 83, 3)");
 change_slider(0, "rgb(188, 225, 237)", "rgb(16, 88, 197)");  // 必须放在最后一个
 
 
@@ -47,15 +47,31 @@ const gotoNum = number => {
     if (current === 0) {
        change_slider(current, "rgb(188, 225, 237)", "rgb(16, 88, 197)");
     }else if (current === 1) {
-        change_slider(current, "rgb(244, 248, 171)", "rgb(92, 97, 0)");
+        change_slider(current, "rgb(152, 209, 188)", "rgb(0, 83, 3)");        
     }else if (current === 2) {
         change_slider(current, "rgb(208, 175, 225)", "rgb(105, 3, 160)");
     }else if (current === 3) {
-        change_slider(current, "rgb(152, 209, 188)", "rgb(0, 83, 3)");
+        change_slider(current, "rgb(244, 230, 154)", "rgb(123, 104, 0)");
     }
 }
 
 function change_slider(index, rgb1, rgb2) {
     root.style.setProperty('--color-background-1', rgb1);
     root.style.setProperty('--color-background-2', rgb2);
+}
+
+function goToImageHtml() {
+    window.open("image.html", "_self");
+}
+
+function goToAnimeHtml() {
+    window.open("anime.html", "_self");
+}
+
+function goToMusicHtml() {
+    window.open("music.html", "_self");
+}
+
+function goToAboutHtml() {
+    window.open("about.html", "_self");
 }
