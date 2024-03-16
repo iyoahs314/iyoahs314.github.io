50c4d763d65d0ac7a4b564643824e359280e1677
iyoahs314 <161724594+iyoahs314@users.noreply.github.com>
2024-03-16 22:06:49 +0800

    1.3.1

--- script/about.js
+++ script/about.js
@@ -3,11 +3,13 @@ var dialogue_box_2 = document.querySelector('.dialogue-box-2');
 var img = document.querySelector('.content-top img');
 
 img.addEventListener('click', function () {
-    if(window.getComputedStyle(dialogue_box).display === "block") {
+    if(window.getComputedStyle(dialogue_box).opacity == 1) {
         console.log("ok");
-        dialogue_box_2.style.display = "block";
+        dialogue_box_2.style.animation = "dialog-appear 2s";
+        dialogue_box_2.style.opacity = 1;
     }
-    dialogue_box.style.display = "block";
+    dialogue_box.style.animation = "dialog-appear 2s";
+    dialogue_box.style.opacity = 1;
 });
 
 function backHome() {
